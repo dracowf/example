3a5262ead05085b03f3a8771e5f6563064303123
DracoWF <DracoWF@gmail.com>
2015-07-22 04:26:47 +0300

    Glow

---
+++ blocks/input/input.bt.js
@@ -0,0 +1,38 @@
+module.exports = function (bt) {
+
+    bt.setDefaultView('input', 'large');
+
+    bt.match('input*', function (ctx) {
+        ctx.enableAutoInit();
+
+        ctx.setContent([
+            {
+                elem: 'control',
+                inputValue: ctx.getParam('value'),
+                inputType: ctx.getParam('type') || 'text',
+                inputName: ctx.getParam('name'),
+                placeholder: ctx.getParam('placeholder')
+            },
+            {
+                elem: 'clear'
+            }
+        ]);
+    });
+
+    bt.match('input*__control', function (ctx) {
+        ctx.setTag('input');
+
+        var currentValue = ctx.getParam('inputValue');
+        var currentName = ctx.getParam('inputName');
+        var currentPlaceholder = ctx.getParam('placeholder');
+
+        ctx.setAttr('value', currentValue);
+        ctx.setAttr('name', currentName);
+        ctx.setAttr('placeholder', currentPlaceholder);
+    });
+
+    bt.match('input*__clear', function (ctx) {
+        // ...
+    });
+
+};
